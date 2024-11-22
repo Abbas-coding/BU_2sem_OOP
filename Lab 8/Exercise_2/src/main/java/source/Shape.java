231278fbf3e39a9d0162f36d7a20b22128cf6056
Abbas-coding <abbasfakhruddin205@gmail.com>
2024-11-22 16:49:20 +0500

    Completed Lab 8 2nd Task

---
+++ Lab 8/Exercise_2/src/main/java/source/Shape.java
@@ -0,0 +1,24 @@
+package source;
+
+public class Shape {
+    private Point p;
+
+    public Shape(Point p){
+        this.p = p;
+    }
+
+    public Point getCenter(){
+        return p;
+    }
+
+    public boolean contains(Point p){
+        double distance = getCenter().distanceTo(p);
+        if(distance > getCenter().distanceTo(this.p)){
+            return false;
+        } else {
+            return true;
+        }
+
+    }
+
+}
