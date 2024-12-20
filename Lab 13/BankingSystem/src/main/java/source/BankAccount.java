8d86117adc1c8aee44ac834ccccee7f50240b4f4
Abbas-coding <abbasfakhruddin205@gmail.com>
2024-12-20 20:04:04 +0500

    Lab 13 Completed

---
+++ Lab 13/BankingSystem/src/main/java/source/BankAccount.java
@@ -0,0 +1,36 @@
+package source;
+
+public class BankAccount {
+    private double balance;
+
+    public BankAccount(double balance) {
+        this.balance = balance;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount) throws InvalidAmountException {
+        if (amount <= 0) {
+            throw new InvalidAmountException("Invalid amount: " + amount);
+        }
+        balance += amount;
+    }
+
+    public void withdraw(double amount) throws InsufficientFundsException, InvalidAmountException {
+        if (amount <= 0) {
+            throw new InvalidAmountException("Invalid amount: " + amount);
+        }
+        if (amount > balance) {
+            throw new InsufficientFundsException("Insufficient funds: " + amount);
+        }
+        balance -= amount;
+    }
+}
+
+
+
+
+
+
---
+++ Lab 13/BankingSystem/src/main/java/source/InvalidAmountException.java
@@ -0,0 +1,7 @@
+package source;
+
+public class InvalidAmountException extends Exception {
+    public InvalidAmountException(String message) {
+        super(message);
+    }
+}
