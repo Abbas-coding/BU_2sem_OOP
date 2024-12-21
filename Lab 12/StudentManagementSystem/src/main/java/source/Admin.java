31f370eaab87703eaea641c1c3e2b9e329050e6b
Abbas-coding <abbasfakhruddin205@gmail.com>
2024-12-21 07:48:49 +0500

    Lab 12 Completed

---
+++ Lab 12/StudentManagementSystem/src/main/java/source/Admin.java
@@ -0,0 +1,10 @@
+package source;
+
+public class Admin {
+    private static final String USERNAME = "admin";
+    private static final String PASSWORD = "password";
+
+    public boolean login(String username, String password) {
+        return USERNAME.equals(username) && PASSWORD.equals(password);
+    }
+}
